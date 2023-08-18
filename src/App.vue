e2e0bbae5ff978cf655e0c411a0233ab8303b69a
Carlos Herrera Iturriaga <50336252+carlx24@users.noreply.github.com>
2023-08-18 11:26:41 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MiCompponente></MiCompponente>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import HelloWorld from './components/HelloWorld.vue'*/
+import MiCompponente from './components/MiCompponente.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MiCompponente
+}
 }
 </script>
 
---
+++ src/components/MiCompponente.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="mi-componente">
+        <h1 v-if="isVisible">{{ message }}</h1>   <!-- Directivas -->
+        <h2 v-bind:class="{ highlighted: isHighlighted }">{{ title }}</h2>
+        <!--<button v-on:click="$event =>isVisible = !isVisible">Ocultar/Mostrar</button>   forma normal -->
+        <button v-on:click="toggleVisibility">Ocultar/Mostrar (m)</button> <!--  Pasando un metodo -->        
+        <input v-model="message">
+        <button v-on:click="sayHello">Alerta</button> <!--  Pasando un metodo -->
+        <button @click="sayHello">Alerta (@)</button> <!--  blazor -->        
+        <button v-on:click="toggleHighlight">otro1</button> <!--  blazor -->        
+        <ul>            
+            <li v-for="item in list" :key = "item.id" >{{ item.id  + ' '+ item.text }}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'MiComponente',
+        data(){
+            return{
+                message: 'Hola mundo desde vue',
+                title: 'Esto es el sub-titulo',
+                isHighlighted: false,
+                isVisible:true,
+                list: [
+                    {id:1, text:'primer item'},
+                    {id:2, text:'segundo item'},
+                    {id:3, text:'tercer item'}
+                ]
+            }
+        },
+        methods: {
+            toggleVisibility(){
+                this.isVisible=!this.isVisible;
+            },
+            sayHello(){
+                alert('Hola desde Vs');
+            },
+            toggleHighlight(){
+                this.isHighlighted=!this.isHighlighted;
+            }
+            
+        },
+    }
+</script>
